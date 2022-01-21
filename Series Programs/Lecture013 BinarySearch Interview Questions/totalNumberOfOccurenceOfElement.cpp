42288de153248cffb3d6671d07730e16fdf7086b
Aakash Garg <aakashgarg1009@gmail.com>
2022-01-21 17:09:22 +0530

    binarySearch pivotElementMountainArray FirstAndLastOccurenceInArray

---
+++ Series Programs/Lecture013 BinarySearch Interview Questions/totalNumberOfOccurenceOfElement.cpp
@@ -0,0 +1,56 @@
+
+//IN O(logn) using binary search as it is pretty easy in O(n)
+int firstOcc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int lastOcc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+//TOTAL COUNT OF THE ELEMENT
+int main(){
+int first=firstOcc(arr,n,k);
+int last=lastOcc(arr,n,k);
+int count=last-first+1;
+    return 0;
+}
+
