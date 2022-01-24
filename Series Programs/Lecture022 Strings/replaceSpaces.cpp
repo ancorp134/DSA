b181cd68f2a6c3f7a88b2d460276f872b32ec3e6
Aakash Garg <aakashgarg1009@gmail.com>
2022-01-24 21:26:50 +0530

    some changes in replaceSpace

--- Series Programs/Lecture022 Strings/replaceSpaces.cpp
+++ Series Programs/Lecture022 Strings/replaceSpaces.cpp
@@ -1,19 +1,35 @@
 #include<string>
 #include<iostream>
 using namespace std;
+
+//MY APPROACH
 string replaceSpaces(string &str){
-	string temp = "";
-    
-    for(int i=0; i<str.length(); i++) {
-        if(str[i] == ' '){
-            temp.push_back('@');
-            temp.push_back('4');
-            temp.push_back('0');
+	// Write your code here.
+      string s;
+        for(int i=0;i<str.length();i++){
+            if(str[i]== ' '){
+                s+="@40";
+                continue;
+            }
+            s+=str[i];
         }
-        else
-        {
-            temp.push_back(str[i]);
-        }
-    }
-    return temp;
+        return s;
 }
+
+
+// string replaceSpaces(string &str){
+// 	string temp = "";
+    
+//     for(int i=0; i<str.length(); i++) {
+//         if(str[i] == ' '){
+//             temp.push_back('@');
+//             temp.push_back('4');
+//             temp.push_back('0');
+//         }
+//         else
+//         {
+//             temp.push_back(str[i]);
+//         }
+//     }
+//     return temp;
+// }
