0e4028274d810f03d5fbbd84ff0a97a6b48f2f14
Aakash Garg <aakashgarg1009@gmail.com>
2022-02-03 17:45:53 +0530

    Updating Excel file

--- Series Programs/Lecture046 ReverseListInKGroups , CheckLLCircular/isCircular.cpp
+++ Series Programs/Lecture046 ReverseListInKGroups , CheckLLCircular/isCircular.cpp
@@ -1,24 +1,26 @@
-// codestudio giving tle for everyone check on GFG
-/*************************************************
-        Following is the structure of class Node:
-     
-        class Node{
-        public:
-            int data;
-            Node* next;
-            
-            Node(int data){
-                this->data = data;
-                this->next = NULL;
-            }
-            
-        }
-**************************************************/
+// codestudio ==> https://www.codingninjas.com/codestudio/problems/is-it-a-circular-linked-list_981265?leftPanelTab=1
+/****************************************************************
 
-bool isCircular(Node* head){
-    // Write your code here.
-    //here the logic written myself is same as of GFG
-    if(head==NULL)
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+bool circularLL(Node* head)
+{
+    // Write your code here
+     if(head==NULL)
         return true;
    
     Node* temp=head->next;
